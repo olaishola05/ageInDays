460c07adfab26f13b30768d94fe3b5db99c3a70e
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-17 00:02:33 +0100

    added the clearInput

--- age.js
+++ age.js
@@ -15,9 +15,6 @@ document.getElementById("ageBtn").addEventListener("click", OurAge);
 
 function OurAge(){
     let value = document.getElementById("options").options[0, 1, 2].text
-    // let option1 = document.getElementById("options").options[0].text
-    // let option2 = document.getElementById("options").options[1].text
-    // let option3 = document.getElementById("options").options[2].text
     if(value[0]){
         let days = ageDiff * 365;  
         alert("my age in " + days+" days")
@@ -31,68 +28,14 @@ function OurAge(){
     }
 }
 
-// let day = function ageInDays(days){
-//     let option = document.getElementById("options").options[0].text
-//     if(option){
-//     days = ageDiff * 365;
-//     // alert("my age in " + days+" days");
-//     }
-// }
-
-// let week = function ageinWk(weeks) {
-//   let option = document.getElementById("options").options[1].text;
-//   if (option) {
-//     weeks = ageDiff * 52;
-//     // alert("your age in " + weeks + " weeks");
-//   }
-// };
-
-// let month = function ageInMonth(months){
-//     let option = document.getElementById("options").options[2].text
-//    if(option){
-//     months = ageDiff * 12;
-//     // alert("my age in " + month+ " months");
-//    }
-// }
-
-
-// function ageinWk(weeks){
-//     document.getElementById("options").options[1].text
-//    weeks = ageDiff * 52
-
-//     // alert("your age in " + weeks + " weeks");
-// }
-
-
-// function ageInMonth(month){
-//     document.getElementById("options").options[2].text
-//     month = ageDiff * 12;
-//     // alert("my age in " + month+ " months");
-// }
-
-// document.getElementById("ageBtn").addEventListener("click", OurAge);
-
-
-
-// function OurAge(ageInDays, ageinWk, ageInMonth){
-
-//     if (document.getElementById("options").options[0].text == ageInDays){
-//         ageIndays = ageInDays();
-//         alert("my age in " + ageInDays)
-//     }
-// // ageType.forEach(element => {
-// //     if (element == OurAge(ageType[0])){
-// //         alert("my age in " + )
-// //     } 
-// //     else if(element === ageType[1]) {
-// //         alert("my age in " + ageType[1])
-// //     } 
-// //     else {
-// //         alert("my age in " + ageType[2])
-// //     }
-// // });
-// }
 
 
 //add clear features after the button has been clicked
 
+document.getElementById("ageBtn").addEventListener("click", clearInput);
+
+
+function clearInput() {
+    document.getElementById("options").value = "";
+    document.getElementById("ageYear").value = "";
+}
