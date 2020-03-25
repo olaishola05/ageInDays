4e198311aafb84adb1a1574194a3c02066f9a85a
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-26 00:13:21 +0100

    changed the contents of ourAge func

--- age.js
+++ age.js
@@ -3,53 +3,43 @@ let currentYear = date.getFullYear();
 let ageDiff = 0;
 
 let getAge = (document.getElementById("ageBtn").onclick = function age() {
-  let userAge = document.getElementById("ageYear").value;
-  ageDiff = currentYear - userAge;
+    let userAge = document.getElementById("ageYear").value;
+    ageDiff = currentYear - userAge;
 
-  alert("my age is " + ageDiff);
+    alert("my age is " + ageDiff);
 });
 
 document.getElementById("ageBtn").addEventListener("click", OurAge);
 
-function OurAge() {
-  let value = document.getElementById("options").options;
-  let picks;
-
-  for (let i = 0; i < value.length; i++) {
-    picks = value[i];
-    if (picks.selected === true) {
-      let days = ageDiff * 365;
-      alert("my age in " + days + " days");
-      break;
-    } else if ((picks.value = "weeks")) {
-      let weeks = ageDiff * 52;
-      alert("your age in " + weeks + " weeks");
-      break;
-    } else if ((picks.text = "Months")) {
-      let months = ageDiff * 12;
-      alert("my age in " + months + " months");
-      break;
+function OurAge(picks) {
+    if (picks.selection.value == "") {
+        alert("Please select an Option");
+        picks.selection.focus();
+        return false;
     }
-  }
-  return picks;
+    if (picks.selection.value == "days") {
+        let days = ageDiff * 365;
+        alert("my age in " + days + " days");
+        picks.selection.focus();
+        return false;
+    }
+    if (picks.selection.value == "weeks") {
+        let weeks = ageDiff * 52;
+        alert("your age in " + weeks + " weeks");
+        picks.selection.focus();
+        return false;
+    }
+    if (picks.selection.value == "months") {
+        let months = ageDiff * 12;
+        alert("my age in " + months + " months");
+        picks.selection.focus();
+        return false;
+    }
 }
-//   for (let i = 0; i < value.options.length; i++) {
-//     // ourPick = value[i];
-//     if (value.options[i].text == "Days") {
-//       let days = ageDiff * 365;
-//       alert("my age in " + days + " days");
-//     } else if (value.options[i].text == "Weeks") {
-//       let weeks = ageDiff * 52;
-//       alert("your age in " + weeks + " weeks");
-//     } else {
-//       let months = ageDiff * 12;
-//       alert("my age in " + months + " months");
-//     }
-//   }
 
 document.getElementById("ageBtn").addEventListener("click", clearInput);
 
 function clearInput() {
-  document.getElementById("options").value = "";
-  document.getElementById("ageYear").value = "";
+    document.getElementById("selectOptions").value = "";
+    document.getElementById("ageYear").value = "";
 }
