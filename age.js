f9d71416e3da1e07409c07c83c0c43f8a5ed50f9
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-26 11:53:23 +0100

    final Version 1.0

--- age.js
+++ age.js
@@ -2,37 +2,52 @@ let date = new Date();
 let currentYear = date.getFullYear();
 let ageDiff = 0;
 
+// let inputVerify = (document.getElementById("ageBtn").onclick = function() {
+//     let userInput = document.getElementById("ageYear");
+
+//     if (userInput == "") {
+//         alert("enter age year in Numeric values");
+//         userInput.focus();
+//         return false;
+//     }
+//     if (userInput == null) {
+//         alert("input cannot be empty");
+//     }
+// });
+
 let getAge = (document.getElementById("ageBtn").onclick = function age() {
     let userAge = document.getElementById("ageYear").value;
     ageDiff = currentYear - userAge;
 
-    alert("my age is " + ageDiff);
+    // alert("my age is " + ageDiff);
 });
 
-document.getElementById("ageBtn").addEventListener("click", OurAge);
+document.getElementById("ageBtn").addEventListener("click", ourAge);
 
-function OurAge(picks) {
-    if (picks.selection.value == "") {
+function ourAge() {
+    let sel = document.getElementById("selectOptions");
+
+    if (sel.value == "") {
         alert("Please select an Option");
-        picks.selection.focus();
+        sel.focus();
         return false;
     }
-    if (picks.selection.value == "days") {
+    if (sel.value == "days") {
         let days = ageDiff * 365;
         alert("my age in " + days + " days");
-        picks.selection.focus();
+        sel.focus();
         return false;
     }
-    if (picks.selection.value == "weeks") {
+    if (sel.value == "weeks") {
         let weeks = ageDiff * 52;
         alert("your age in " + weeks + " weeks");
-        picks.selection.focus();
+        sel.focus();
         return false;
     }
-    if (picks.selection.value == "months") {
+    if (sel.value == "months") {
         let months = ageDiff * 12;
         alert("my age in " + months + " months");
-        picks.selection.focus();
+        sel.focus();
         return false;
     }
 }
